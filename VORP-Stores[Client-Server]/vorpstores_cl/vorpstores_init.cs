e677b7e37e70b845067fb5d59a7e7110d2165974
Osmany González <devbiitz2@gmail.com>
2020-06-01 13:40:40 +0100

    First Init (Configs and Libs)

---
+++ VORP-Stores[Client-Server]/vorpstores_cl/vorpstores_init.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vorpstores_cl
+{
+    public class vorpstores_init
+    {
+    }
+}
