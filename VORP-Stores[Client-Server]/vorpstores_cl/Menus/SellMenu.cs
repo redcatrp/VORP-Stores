09fa92884b66d143c48be3bf4fb080db911395fe
Osmany González <devbiitz2@gmail.com>
2020-06-02 16:17:46 +0100

    First Cs SellMenu

---
+++ VORP-Stores[Client-Server]/vorpstores_cl/Menus/SellMenu.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vorpstores_cl.Menus
+{
+    class SellMenu
+    {
+    }
+}
